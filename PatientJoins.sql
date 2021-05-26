6235c569a59c33095371c327d156fd3db822ff46
derryoloan <65630365+derryoloan@users.noreply.github.com>
2021-05-26 17:53:55 +0100

    Add files via upload

---
+++ PatientJoins.sql
@@ -0,0 +1,4 @@
+﻿select Patient.PatientName,Patient_RiskFactors.RiskFactor,Patient_MedicalSurgicalHistory.MedicalSurgicalHistory,Patient_FamilyHistory.FamilyHistory
+from Patient left outer join Patient_RiskFactors on patient.MRN=Patient_RiskFactors.MRN left outer join Patient_MedicalSurgicalHistory on Patient.MRN=Patient_MedicalSurgicalHistory.MRN
+left outer join Patient_FamilyHistory on Patient.MRN=Patient_FamilyHistory.MRN
+ where Patient.Mrn ='12345'
---
+++ TypeLabs.sql
@@ -0,0 +1,7 @@
+﻿Create type [dbo].[labTable] as table (
+[Symphony] [varchar] (50) null,
+[LabDate] [varchar] (50) null,
+[LabTest] [varchar] (50) null,
+[LabResult] [varchar] (50) null
+)
+go
